e4e3a822a324af385459aefab4b7e550f4af5eff
William Annis <wannis@fsu.edu>
2026-06-27 23:14:54 -0400

    seperated functions to new files

---
+++ R/len_r2.R
@@ -0,0 +1,77 @@
+# len_R2  ----------------------------------------------------------------------
+
+#' R-squared estimation for length-at-age predictions
+#' 
+#' @description Estimates r-squared of length predictions of all candidate or
+#' stacked models. 
+#' 
+#' @inheritParams stack_args
+#' @param data Data.frame containing actual length-at-age data with appropriate
+#' sampling id. Must have columns: age, length, and sample_id.
+#' @param stack T or F. Create model stacked predictions or parameter estimates
+#' (T), or candidate model specific outputs (F). Default is FALSE
+#' @param sp Character for name of species of which to estimate r-squared
+#'
+#' @details Function predicts the length at age for each random grouping. 
+#' R-squared is then estimated for each candidate or the stacked model. 
+#' 
+#' @returns Data.frame containing r-squared and adjusted r-squared for each 
+#' model
+#' @export
+
+len_R2 <- function(
+    stack.df,
+    mod.dir,
+    sim,
+    sum.fun,
+    data,
+    stack
+    ) {
+  
+  # model names
+  if(stack) mods <- "stacked"
+  if(!stack) mods <- stack.df$model
+  
+  # Predict length at age
+  pred_df <-curve_predictR(
+    stack.df = stack.df, 
+    mod.dir = mod.dir,
+    type = "prediction",
+    group.id = "site",
+    pred.input = data$age,
+    pred.group = data$sample_id,
+    stack=stack,
+    sim=sim,
+    sum.fun = sum.fun,
+    input.var = "age",
+    output.var = "length")
+  
+  # Link predictions to actual data
+  linked_df <- pred_df %>% 
+    dplyr::left_join(
+      data,
+      by = dplyr::join_by(sample_id, age),
+      multiple = "first"
+    )
+  
+  # R-squared function
+  r2_list <- lapply(mods, function (x) {
+    
+    # subset data for select mode
+    if(stack) df <- linked_df 
+    if(!stack) df <- linked_df %>% dplyr::filter(mod == x)
+    
+    # assign mean or median prediction
+    if(sum.fun == "mean") df$pred <- df$length_pred_mean
+    if(sum.fun == "median") df$pred <- df$length_pred_median
+    
+    # Estimate r2
+    out <- lm(pred ~ length,df)
+    data.frame(
+      model = x,
+      r2 = summary(out)$r.squared, 
+      adj_r2 = summary(out)$adj.r.squared
+    )
+  }) 
+  dplyr::bind_rows(r2_list)
+}
---
+++ R/linear_pred_stackR.R
@@ -0,0 +1,105 @@
+# linear_pred_stackR  ----------------------------------------------------------
+
+#' Model-stacked instantaneous growth predictions
+#'
+#' @description Creates model-stacked predictions of instantaneous growth rate
+#' across a range of predictor variables.
+#' 
+#' @inheritParams stack_args
+#'
+#' @details Function extract prediction posterior distributions of instantaneous 
+#' growth rates across predictors based on model names provided with stacking 
+#' weights. Posterior distributions are combined based on stacking weights and 
+#' summarized with mean or median, and 95% credible intervals. Wrapper for 
+#' .predict_extract, linear_prediction_sampler.
+#' 
+#' @returns Data.frame with rows for each prediction input (e.g. PC1 value) and
+#' columns for prediction number (pred_id), and columns for prediction summary 
+#' statistics (mean or median, lower-lwr, and upper-upr credible interval) for 
+#' each output variable.
+#' 
+#' @export
+
+
+linear_pred_stackR <- function(stack.df, mod.dir, sim,sum.fun){
+  
+  # Retain models with 
+  stack <- stack.df %>% 
+    dplyr::mutate(n_sim = round(sim*stack_wt)) %>% 
+    dplyr::filter(n_sim >0)
+  
+  # Load it model parameters
+  mods<- stack$model
+  sim_list = stack$n_sim
+  
+  # Extract posterior distributions of all models with stacking weights
+  mod_list <- lapply(mods,.predict_extract,mod.dir)
+  
+  # Run prediction function
+  pred_list <- purrr::map2(mod_list,sim_list, .linear_predict_sampler)
+  
+  # Bind results into 3d array
+  out <- abind::abind(pred_list, along = 3)
+  
+  # Summarize into data.frame
+  .boot_summary(
+    out,
+    sum.fun=sum.fun,
+    group.var=c("pred_id")
+  )
+}
+  
+# Prediction extracting helpers  -----------------------------------------------
+
+# Extracts posterior distribution of the  predictions of the asymptotic
+# and inflection parameters, as well as instantaneous growth predictions given
+# specific values of predictors. Predictions are created in Stan model and 
+# these functions extracts these predictions into a 3d array (number of 
+# predictions, n.parameters X linear predictors, iterations). Each slice is a 
+# posterior draw of the prediction.
+
+
+# extract posterior from model output
+.predict_extract <- function(mod.out,mod.dir) {
+  
+  # Load in model
+  mod <- readRDS(file.path(mod.dir,mod.out))
+  
+  # Select predictions of parameters in actual model
+  all_param <- c("pred_Linf","pred_ig")
+  param <- all_param[all_param %in% mod@model_pars]
+  
+  # Extract posteriors
+  sim_list <- rstan::extract(mod,param)
+  
+  # Format each array to bind into one
+  pred_list <- purrr::map2(sim_list,names(sim_list), function(x,y) {
+    array <- aperm(x,c(2,3,1))  # format array so slices are draws
+    colnames(array) <- sapply(seq_len(ncol(array)), function(i) paste0(y,i))  # give each column a unique name
+    array
+  }
+  )
+  
+  # Bind into one array
+  pred_array <-abind::abind(pred_list,along = 2)
+  
+  # Create a column for prediction id (used for boot summary function)   
+  id <- array(rep(seq(1,nrow(pred_array)),dim(pred_array)[3]),
+              dim = c(dim(pred_array)[1],1,dim(pred_array)[3]),
+              dimnames = list(NULL,"pred_id",NULL)) 
+  abind::abind(id,pred_array,along=2)
+  
+  
+}  
+
+# Selects random draws
+.linear_predict_sampler <- function (model.out,n.sim) {
+  
+  # Pull random samples from posterior
+  mod_list <- .post_draw(model.out,n.sim)
+  
+  # Extract parameter of interest
+  out_list <- lapply(mod_list, function(x) as.data.frame(x))
+  abind::abind(out_list,along = 3)
+  
+}
